ae863e374fd07e5744617b36c9718b31d47eb35e
IoanamMihai <ioanamihai2211@gmail.com>
2023-05-02 14:35:09 +0300

    Frontend

---
+++ js/components/MainPage.jsx
@@ -0,0 +1,68 @@
+// js/components/MainPage.jsx
+import {useEffect, useState} from "react";
+
+export default function MainPage() {
+    const [records, setRecords] = useState([]);
+
+    useEffect(() => {
+        try{
+            fetch('/api/records', {
+                method: 'GET',
+            })
+                .then(response => response.json())
+                .then(json => setRecords(json.data));
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }, []);
+
+    const deleteRecord = (event) => {
+        event.preventDefault();
+        const id = event.target.id;
+        try {
+            fetch(`/api/records?id=${id}`, {
+                method: 'DELETE',
+            })
+                .then(response => response.json())
+                .then(json => {
+                    setRecords(records.filter(record => record._id !== id));
+                });
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+
+        <section className="bg-red-300 dark:bg-blue-500">
+            <div className="container px-6 py-10 mx-auto">
+                <h1 className="  w-[1000px] mx-auto  font-bold text-center text-3xl text-pink-700">Nail Technician Feedback</h1>
+                <p className="  w-[1000px] mx-auto text-center mt-4 text-2xl text-pink-900">Hello girls! My name is Ioana and this is my work seen through my customers eyes. Enjoy! :)</p>
+
+                <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
+                    {records.map(record => (
+                          <div  key={record._id}
+                            className="max-w-sm p-6 bg-blue-300 border border-blue-300 rotate-2 rounded-ee-full shadow hover:bg-pink-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
+                          >
+                            <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                {record.nume}
+                            </h3>
+                            <p className="font-serif text-gray-700 dark:text-gray-400">
+                                {record.prenume}
+                            </p>
+                              <div className={"flex justify-center mt-4"}>
+                                  <button type="button"
+                                          id={record._id}
+                                          onClick={deleteRecord}
+                                          className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete
+                                  </button>
+                              </div>
+                          </div>
+                    ))
+                        }
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ pages/insert.jsx
@@ -0,0 +1,49 @@
+// js/components/InsertPage.jsx
+export default function InsertPage() {
+    const insertRecord = (event) => {
+        event.preventDefault();
+        const nume = document.getElementById("nume").value;
+        const prenume = document.getElementById("prenume").value;
+        const data = {nume, prenume};
+        fetch("/api/records", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then(() => {
+            console.log("New record inserted");
+            document.getElementById("nume").value = "";
+            document.getElementById("prenume").value = "";
+        });
+    }
+
+    return (
+        <section className="bg-red-300 dark:bg-gray-900">
+            <div className="container px-6 py-10 mx-auto">
+                <h1 className="w-[500px] mx-auto text-center text-6xl text-pink-700">Nail Technician Feedback</h1>
+                <p className="w-[1000px] mx-auto text-center mt-4 text-3xl text-pink-900">Here is your chance to tell everybody and Ioana how was your experience!</p>
+
+                <form>
+                    <div className="mb-6">
+                        <label htmlFor="nume" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name is...</label>
+                        <input type="text" id="nume"
+                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                               placeholder="Ioana Mihai" required/>
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="prenume"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your feedback...</label>
+                        <textarea id="prenume"
+                                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                  required/>
+                    </div>
+                    <button type="submit"
+                            onClick={ insertRecord }
+                            className="text-white bg-red-700 hover:bg-pink-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
+                    </button>
+                </form>
+            </div>
+        </section>
+    )
+}
